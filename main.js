02694f5ea00b010134c78e3facdfef5714d5a63f
bharatvitkar <79301656+bharatvitkar@users.noreply.github.com>
2022-12-18 09:39:27 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -8,10 +8,32 @@ function setup() {
 	canvas = createCanvas(1240,336);
 	canvas.parent('canvas');
 	instializeInSetup(mario);
+	video=createCapture(VIDEO);
+	video.size(800,300);
+	video.parent('game_console');
+	poseNet=ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose',gotPoses);
 }
 
-function draw() {
-	game()
+function modelLoaded()
+{
+	console.log('model loaded');
+}
+
+function gotPoses(results)
+{
+	if(results.length>0)
+	{
+		console.log(results);
+		noseX=results[0].pose.nose.X;
+		noseY=results[0].pose.nose.Y;
+	}
+}
+
+function draw() 
+{
+	game();
+
 }
 
 
